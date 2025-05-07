3d0a549b037778939d60fadca9596c10fc2aa8b1
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-05-07 11:03:17 -0700

    Update script.js

--- apex/script.js
+++ apex/script.js
@@ -1,7 +1,4 @@
-
-  window.addEventListener('mousemove', (e) => {
-            const x = (e.clientX / window.innerWidth - 0.5) * 15;
-            const y = (e.clientY / window.innerHeight - 0.5) * 15;
+     const y = (e.clientY / window.innerHeight - 0.5) * 15;
             document.querySelector('.landing-content').style.transform = `translate(${x}px, ${y}px)`;
         });
     }
